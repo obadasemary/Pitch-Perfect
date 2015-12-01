8b503cc9e7ad9b2b415e638dba55d92dcbaea146
Abdelrahman Mohamed <obada.semary@gmail.com>
2015-12-01 17:05:09 +0300

    1.1
    Recording Design & Stop Button Audio

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        print("recordAudio")
+        recordingInProgress.hidden = false
+    }
 
+    @IBAction func stopAudio(sender: UIButton) {
+        print("stopAudio")
+        recordingInProgress.hidden = true
+    }
 }
 
